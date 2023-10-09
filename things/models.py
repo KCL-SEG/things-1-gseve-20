1917c0481ca6f0304010e40adfa8c06ccf5d7bdd
Gabriela Sevelkova <gsevelkova5@gmail.com>
2023-10-09 15:59:51 +0100

    Correcting typo error for description field

--- things/models.py
+++ things/models.py
@@ -3,5 +3,5 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
-    desciption = models.CharField(max_length=120, blank=True, unique=False)
+    description = models.CharField(max_length=120, blank=True, unique=False)
     quantity = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(100)])
