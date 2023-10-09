c06e68f2b42aa4ed99e1f455f210a9fc5bebcaaf
Gabriela Sevelkova <gsevelkova5@gmail.com>
2023-10-09 15:55:12 +0100

    Adding boundaries to Model Fields in Thing

--- things/models.py
+++ things/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Thing(models.Model):
-    name = models.CharField(max_length=40)
-    desciption = models.CharField(max_length=400)
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    desciption = models.CharField(max_length=120, blank=True, unique=False)
+    quantity = models.IntegerField(min=0, max=100)
