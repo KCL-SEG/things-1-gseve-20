5a976d013e8ccd0a0be5e6d4819180e449800186
Gabriela Sevelkova <gsevelkova5@gmail.com>
2023-10-09 15:58:33 +0100

    Adding validators to qualitity field

--- things/models.py
+++ things/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     desciption = models.CharField(max_length=120, blank=True, unique=False)
-    quantity = models.IntegerField(min=0, max=100)
+    quantity = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(100)])
